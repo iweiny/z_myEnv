3058b2493f69ca804357e86d858c9af8087b1303
Ira Weiny <ira.weiny@intel.com>
2015-03-04 01:42:23 -0500

    setup-rem-machine.sh: sets up ssh keys and clones basic env.

---
+++ bin/setup-rem-machine.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+if [ "$1" == "" ]; then
+	echo "Usage: setup-rem-machine.sh <host>"
+	echo "       Clone env to host and set up ssh keys there"
+	exit 1
+fi
+
+base_host="`hostname`.`domainname`"
+remote=$1
+
+pushd `pwd`/..
+my_env_dir=`pwd`
+popd
+
+echo ""
+echo "Generating key on localhost"
+echo ""
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+	ssh-keygen -t rsa -f ~/.ssh/id_rsa
+fi
+pub_key=`cat ~/.ssh/id_rsa.pub`
+
+echo ""
+echo "Generating key on $remote"
+echo ""
+
+ssh $remote << EOF
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+	ssh-keygen -t rsa -f ~/.ssh/id_rsa
+fi
+EOF
+
+echo ""
+echo "Authorizing localhost on $remote:"
+echo ""
+echo $pub_key
+echo ""
+
+cmd="echo '$pub_key' >> ~/.ssh/authorized_keys"
+ssh $remote $cmd
+
+rem_pub_key=`ssh $remote 'cat ~/.ssh/id_rsa.pub'`
+
+echo ""
+echo "Authorizing $remote on localhost:"
+echo ""
+echo $rem_pub_key
+echo ""
+
+echo $rem_pub_key >> ~/.ssh/authorized_keys
+
+
+echo ""
+echo "Cloning: $base_host:$my_env_dir"
+echo ""
+
+echo " Run this and exit when done"
+echo ""
+echo "   git clone -b master $base_host:$my_env_dir"
+echo ""
+ssh $remote
+
+ssh $remote 'pushd ~/z_myEnv; ./setup.sh'
+
+
